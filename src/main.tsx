b6e3e4c93cb6cbc79a7ff57f32105b733b7c1eb7
Freddy Garcia <tu-correo@ejemplo.com>
2026-03-14 21:17:42 -0500

    prueba netlify

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App';
-import './styles/globals.css' // Verifica que esta ruta a tu CSS global sea correcta
 
+// Prueba mínima:
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <h1>Hola desde Netlify 🚀</h1>
 )
